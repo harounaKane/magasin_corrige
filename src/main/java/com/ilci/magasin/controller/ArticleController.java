c46e62b84de1bc77dfb43a07d44a0183c193e02c
harounaKane <harouna.kane@hotmail.fr>
2023-05-12 11:26:28 +0200

    action (modifi/delete) sur produit. close msg flash

--- src/main/java/com/ilci/magasin/controller/ArticleController.java
+++ src/main/java/com/ilci/magasin/controller/ArticleController.java
@@ -6,9 +6,11 @@ import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ilci.magasin.MyServices;
 import com.ilci.magasin.entity.Produit;
@@ -49,12 +51,33 @@ public class ArticleController {
 
 			produit.setImage(fileName);
 			
-			produitRepository.save(produit);
-			return "redirect:/article/articles";
 		}
-
 		
-		return "articles/index";
+	//	produitRepository.save(produit);
+	//	System.out.println(logo);
+		System.out.println(produit);
+		return "redirect:/article/articles";
+		
+	}
+	
+	@GetMapping("/article/update/{id}")
+	public String update( ModelMap model, @PathVariable int id) {
+
+		Produit article = produitRepository.findById(id).get();
+		
+		System.out.println("art " + article);
+		model.addAttribute("article", article);
+		
+		return "articles/new";
+	}
+	
+	@GetMapping("/article/delete/{id}")
+	public String delete(RedirectAttributes redirectAttributes, Produit article) {
+		produitRepository.delete(article);
+		System.out.println(article);
+		redirectAttributes.addFlashAttribute("success", "article supprimé avec succès ! ");
+		
+		return "redirect:/article/articles";
 	}
 
 }
